e35f645287c8fa8c35779906d5c074eb9d0a0235
draeriel <danieljuanbarbosa@gmail.com>
2018-05-21 22:21:09 +0200

    acceso a array text, mostrando sin comas usando join

--- main.js
+++ main.js
@@ -36,10 +36,12 @@ $(document).ready(function() {
             }
             if (key == "text") {
                 for (var frase = 0; frase < data[counter].text.length; frase++){
-                    $(parrafo).append(items.push("<p>" + data[counter].text[frase] + "</p>"))                    
+                    var element = '<p>'+data[counter].text[frase]+'</p>';
+                    parrafo.push(element) 
+                    parrafo.join("");
+                    console.log(parrafo);                 
                 };
-                console.log(parrafo);
-                items.push("<div class='modal fade' id='" + id + "' role='dialog'> <div class='modal-dialog modal-lg'><div class='modal-content'><div class='modal-header'><h4 class='modal-title'>" + title + "</h4></div><div class='modal-body'>" + parrafo + "</div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div>");
+                items.push("<div class='modal fade' id='" + id + "' role='dialog'> <div class='modal-dialog modal-lg'><div class='modal-content'><div class='modal-header'><h4 class='modal-title'>" + title + "</h4></div><div class='modal-body'>" + parrafo.join("") + "</div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div>");
                 parrafo = []
             }
             if (key == "date") {
