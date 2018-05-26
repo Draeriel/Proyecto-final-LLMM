b724c214e97f80a130024742f95bdf4463a69591
draeriel <danieljuanbarbosa@gmail.com>
2018-05-26 19:08:34 +0200

    añadidos json extra, mejorado el estilo

--- main.js
+++ main.js
@@ -7,59 +7,93 @@ $(document).ready(function () {
     win.scroll(function () {
         if ($(document).height() - win.height() == win.scrollTop()) {
             if (!done) {
-                    $.getJSON("main.json", function (data) {
+                $.getJSON("main.json", function (data) {
+                    var items = [];
+                    var title = '';
+                    var id = '';
+                    var parrafo = [];
+                    for (var counter = 0; counter < data.length; counter++) {
+                        $.each(data[counter], function (key, val) {
+                            if (key == "title") {
+                                title = val;
+                                items.push("<header><h1 class='" + key + "'>" + val + "</h1></header>")
+                            }
+
+                            if (key == "id") {
+                                id = val;
+
+                            }
+
+                            if (key == "imgbig") {
+
+                                items.push("<img src='" + val + "'data-toggle='modal' data-target='#" + id + "' />")
+                            }
+                            if (key == "text") {
+                                for (var frase = 0; frase < data[counter].text.length; frase++) {
+                                    var element = '<p>' + data[counter].text[frase] + '</p>';
+                                    parrafo.push(element)
+                                    parrafo.join("");
+                                };
+                                items.push("<div class='modal fade' id='" + id + "' role='dialog'> <div class='modal-dialog modal-lg'><div class='modal-content'><div class='modal-header'><h4 class='modal-title'>" + title + "</h4></div><div class='modal-body'>" + parrafo.join("") + "</div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Cerrar</button></div></div></div></div>");
+                                parrafo = []
+                            }
+                            if (key == "date") {
+                                items.push("<p class='" + key + "'>" + val + "</li>");
+                                $("<article/>", {
+                                    "class": "article",
+                                    html: items.join("")
+                                }).appendTo("#posts");
+                                items = [];
+                            }
+
+                        });
+                    };
+
+
+                });
+            }
+            done = true;
+            if (!isButton) {
+                var boton = document.createElement("button");
+                boton.setAttribute("id", "hex")
+                boton.addEventListener("click", function () {
+                    document.getElementById("hex").style.display = "none";
+                });
+
+
+                boton.innerHTML = "Abrir Hexeosis";
+                var body = document.getElementsByTagName("body")[0];
+                body.appendChild(boton);
+                boton.addEventListener("click", function () {
+                    $.getJSON("hexeosis.json", function (data) {
                         var items = [];
-                        var title = '';
-                        var id = '';
-                        var parrafo = [];
                         for (var counter = 0; counter < data.length; counter++) {
                             $.each(data[counter], function (key, val) {
-                                if (key == "title") {
-                                    title = val;
-                                    items.push("<header><h1 class='" + key + "'>" + val + "</h1></header>")
-                                }
-
-                                if (key == "id") {
-                                    id = val;
-
-                                }
-
-                                if (key == "imgbig") {
-
-                                    items.push("<img src='" + val + "'data-toggle='modal' data-target='#" + id + "' />")
-                                }
-                                if (key == "text") {
-                                    for (var frase = 0; frase < data[counter].text.length; frase++) {
-                                        var element = '<p>' + data[counter].text[frase] + '</p>';
-                                        parrafo.push(element)
-                                        parrafo.join("");
-                                    };
-                                    items.push("<div class='modal fade' id='" + id + "' role='dialog'> <div class='modal-dialog modal-lg'><div class='modal-content'><div class='modal-header'><h4 class='modal-title'>" + title + "</h4></div><div class='modal-body'>" + parrafo.join("") + "</div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Cerrar</button></div></div></div></div>");
-                                    parrafo = []
-                                }
-                                if (key == "date") {
-                                    items.push("<p class='" + key + "'>" + val + "</li>");
+                                if (key == "img") {
+                                    items.push("<img src='" + val + "' />")
                                     $("<article/>", {
                                         "class": "article",
                                         html: items.join("")
                                     }).appendTo("#posts");
+
                                     items = [];
                                 }
-
                             });
                         };
 
-
                     });
-            }
-            done = true;
-                if (!isButton) {
-                    var button = document.createElement("button");
-                    button.innerHTML = "Abrir Hexeosis";
+                    var boton = document.createElement("button");
+                    boton.setAttribute("id", "carrero")
+                    boton.addEventListener("click", function () {
+                        document.getElementById("carrero").style.display = "none";
+                    });
+
+
+                    boton.innerHTML = "Memes Carrero Blanco";
                     var body = document.getElementsByTagName("body")[0];
-                    body.appendChild(button);
-                    button.addEventListener("click", function () {
-                        $.getJSON("hexeosis.json", function (data) {
+                    body.appendChild(boton);
+                    boton.addEventListener("click", function () {
+                        $.getJSON("carrero.json", function (data) {
                             var items = [];
                             for (var counter = 0; counter < data.length; counter++) {
                                 $.each(data[counter], function (key, val) {
@@ -75,9 +109,48 @@ $(document).ready(function () {
                                 });
                             };
                         });
+                        var boton = document.createElement("button");
+                    boton.setAttribute("id", "youtube")
+                    boton.addEventListener("click", function () {
+                        document.getElementById("youtube").style.display = "none";
                     });
-                    isButton = true;
-                }
+
+
+                    boton.innerHTML = "Videos Numberphile";
+                    var body = document.getElementsByTagName("body")[0];
+                    body.appendChild(boton);
+                    boton.addEventListener("click", function () {
+                        $.getJSON("youtube.json", function (data) {
+                            var items = [];
+                            for (var counter = 0; counter < data.length; counter++) {
+                                $.each(data[counter], function (key, val) {
+                                    if (key == "title") {
+                                        title = val;
+                                        items.push("<header><h1 class='" + key + "'>" + val + "</h1></header>")
+                                    }
+                                    if (key == "youtube") {
+                                        items.push(val)
+                                        $("<article/>", {
+                                            "class": "article",
+                                            html: items.join("")
+                                        }).appendTo("#posts");
+
+                                        items = [];
+                                    }
+                                });
+                            };
+                        });
+                    });
+                    });
+                    
+                });
+
+                isButton = true;
+            }
         }
     });
 });
+
+function cerrar() {
+    document.getElementById("ban").style.display = "none";
+}
