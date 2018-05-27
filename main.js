f90286835355316a4d412286706be1e076b16c57
draeriel <danieljuanbarbosa@gmail.com>
2018-05-27 13:02:39 +0200

    rss uri charger

--- main.js
+++ main.js
@@ -3,6 +3,91 @@ $(document).ready(function () {
     var done = false;
     let buttonnode;
     var isButton = false;
+    
+
+    let getUrlParameter = function getUrlParameter(sParam) {
+        let sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;    for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+                    if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? false : sParameterName[1];
+            }
+        }
+    };
+    let page = getUrlParameter('page');
+    let news = getUrlParameter('news');
+
+
+    if (page && news) {
+        $('#posts').empty();
+        $.getJSON( page + ".json", function (data) {
+                var items = [];
+                var title = '';
+                var id = '';
+                var parrafo = [];
+                    $.each(data[news], function (key, val) {
+                        if (key == "title") {
+                            title = val;
+                            items.push("<header><h1 class='" + key + "'>" + val + "</h1></header>")
+                        }
+
+                        if (key == "id") {
+                            id = val;
+
+                        }
+
+                        if (key == "imgbig") {
+
+                            items.push("<img src='" + val + "'data-toggle='modal' data-target='#" + id + "' />")
+                        }
+                        if (key == "text") {
+                            for (var frase = 0; frase < data[news].text.length; frase++) {
+                                var element = '<p>' + data[news].text[frase] + '</p>';
+                                parrafo.push(element)
+                                parrafo.join("");
+                            };
+                            items.push("<div class='modal fade' id='" + id + "' role='dialog'> <div class='modal-dialog modal-lg'><div class='modal-content'><div class='modal-header'><h4 class='modal-title'>" + title + "</h4></div><div class='modal-body'>" + parrafo.join("") + "</div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Cerrar</button></div></div></div></div>");
+                            parrafo = []
+                        }
+                        if (key == "img") {
+                            items.push("<img src='" + val + "' />")
+                            $("<article/>", {
+                                "class": "article",
+                                html: items.join("")
+                            }).appendTo("#posts");
+
+                            items = [];
+                        }
+                        if (key == "video") {
+                            items.push(val)
+                            $("<article/>", {
+                                "class": "article",
+                                html: items.join("")
+                            }).appendTo("#posts");
+
+                            items = [];}
+                            
+                        if (key == "date") {
+                            items.push("<p class='" + key + "'>" + val + "</li>");
+                            $("<article/>", {
+                                "class": "article",
+                                html: items.join("")
+                            }).appendTo("#posts");
+                            items = [];
+                        }
+
+                    });
+
+
+
+            });
+            done = true;
+        }
+        
+
+
 
     win.scroll(function () {
         if ($(document).height() - win.height() == win.scrollTop()) {
